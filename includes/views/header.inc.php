44f1ff3621f71528292824acf7ce8c3ce91bbed8
David Alvarado <dalvarado0@ucol.mx>
2018-11-02 20:38:49 -0600

    #1 Agrega al listado principal de personas el campo “Dirección” y quitar el campo “Fecha”

--- includes/views/header.inc.php
+++ includes/views/header.inc.php
@@ -4,7 +4,7 @@
 <head>
 	<title>Simple cat&aacute;logo </title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="<?php echo $url;?>assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 <head>
 	
 <body class="page">
